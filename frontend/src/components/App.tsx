a4f2503e50d300753121b8fb66d3498b75e8b74a
nasum <tomato.wonder.life@gmail.com>
2021-02-14 22:53:37 +0900

    split header component

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
+import { AppHeader } from './AppHeader'
 
 export const App: React.FC = () => {
   return (
     <div className="relative bg-white">
-      <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <h1 className="text-white">Spin</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <AppHeader />
     </div>
   )
 }
