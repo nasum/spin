929aa944551166cb68cbab493bf65b659b0a2215
nasum <tomato.wonder.life@gmail.com>
2021-03-01 16:34:18 +0900

    split router

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"text/template"
-
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/nasum/spin/route"
 )
 
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
 func main() {
-	t := &Template{
-		templates: template.Must(template.ParseGlob("dist/*.html")),
-	}
-
-	e := echo.New()
-
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	e.Renderer = t
-	e.Static("/js", "dist/js")
-	e.GET("*", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "index.html", "")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	route.Init()
 }
---
+++ route/router.go
@@ -0,0 +1,36 @@
+package route
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func Init() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("dist/*.html")),
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Renderer = t
+	e.Static("/js", "dist/js")
+	e.GET("*", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", "")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
