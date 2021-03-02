b6f10e7ddea69b8278a677c86f056ecb3362d1a4
nasum <tomato.wonder.life@gmail.com>
2021-03-02 23:11:12 +0900

    connect postgresql

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/nasum/spin/constants"
 	"github.com/nasum/spin/route"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
 )
 
 func main() {
+	url := constants.GetEnv().DataBaseURL
+	sqlDB, err := sql.Open("postgres", url)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(-1)
+	}
+
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		Conn: sqlDB,
+	}), &gorm.Config{})
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(-1)
+	}
+
+	db.AutoMigrate()
+
 	route.Init()
 }
--- constants/env.go
+++ constants/env.go
@@ -8,17 +8,20 @@ type Env struct {
 	TwitterAPIKey       string
 	TwitterAPIKeySecret string
 	CallbackURL         string
+	DataBaseURL         string
 }
 
 func GetEnv() Env {
 	twitterAPIKey := os.Getenv("TWITTER_API_KEY")
 	twitterAPIKeySecret := os.Getenv("TWITTER_API_KEY_SECRET")
 	callbackURL := os.Getenv("CALLBACK_URL")
+	databaseURL := os.Getenv("DATABASE_URL")
 
 	e := Env{
 		TwitterAPIKey:       twitterAPIKey,
 		TwitterAPIKeySecret: twitterAPIKeySecret,
 		CallbackURL:         callbackURL,
+		DataBaseURL:         databaseURL,
 	}
 
 	return e
