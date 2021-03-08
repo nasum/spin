bcae8716072634a742d322c442adffcfb32f2368
nasum <tomato.wonder.life@gmail.com>
2021-03-09 00:22:17 +0900

    save user and twitter_access_token

--- handler/oauth.go
+++ handler/oauth.go
@@ -10,6 +10,8 @@ import (
 	twitterOAuth1 "github.com/dghubble/oauth1/twitter"
 	"github.com/labstack/echo/v4"
 	"github.com/nasum/spin/constants"
+	"github.com/nasum/spin/infrastructure"
+	"github.com/nasum/spin/model"
 )
 
 var Config *oauth1.Config
@@ -45,7 +47,7 @@ func SignUp() echo.HandlerFunc {
 func Callback() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		ctx := c.Request().Context()
-		//conn := infrastructure.Connection
+		conn := infrastructure.Connection
 
 		e := constants.GetEnv()
 		config := oauth1.Config{
@@ -58,24 +60,24 @@ func Callback() echo.HandlerFunc {
 		requestToken, verifier, err := oauth1.ParseAuthorizationCallback(c.Request())
 
 		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 			return echo.NewHTTPError(http.StatusInternalServerError, "Can not create twitter request token")
 		}
 
 		_, requestTokenSecret, err := config.RequestToken()
 
 		if err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
 			return echo.NewHTTPError(http.StatusInternalServerError, "Can not create twitter request token")
 		}
 
-		accessToken, accessSecret, err := config.AccessToken(requestToken, requestTokenSecret, verifier)
-		fmt.Println(accessToken)
-		fmt.Println(accessSecret)
+		accessToken, accessSeacret, err := config.AccessToken(requestToken, requestTokenSecret, verifier)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
-			os.Exit(-1)
+			return echo.NewHTTPError(http.StatusInternalServerError, "Can not create twitter accessToken")
 		}
 
-		httpClient := config.Client(ctx, oauth1.NewToken(accessToken, accessSecret))
+		httpClient := config.Client(ctx, oauth1.NewToken(accessToken, accessSeacret))
 		twitterClient := twitter.NewClient(httpClient)
 
 		accountVerifyParams := &twitter.AccountVerifyParams{
@@ -84,14 +86,23 @@ func Callback() echo.HandlerFunc {
 			IncludeEmail:    twitter.Bool(false),
 		}
 
-		user, _, err := twitterClient.Accounts.VerifyCredentials(accountVerifyParams)
+		account, _, err := twitterClient.Accounts.VerifyCredentials(accountVerifyParams)
 
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
-			os.Exit(-1)
+			return echo.NewHTTPError(http.StatusInternalServerError, "Can not get twitter user")
 		}
 
-		fmt.Println(user.ScreenName)
+		user := model.User{
+			Name: account.ScreenName,
+		}
+		twitterToken := model.TwitterToken{
+			AccessToken:        accessToken,
+			AccessTokenSeacret: accessSeacret,
+			User:               user,
+		}
+
+		conn.Create(&twitterToken)
 
 		return nil
 	}
--- model/twitter_token.go
+++ model/twitter_token.go
@@ -1,16 +1,9 @@
 package model
 
-import (
-	"gorm.io/gorm"
-	"time"
-)
-
 type TwitterToken struct {
-	Id                uint      `json:"id`
-	UserId            uint      `json:"user_id`
-	User              User      `json:"user`
-	AccessToken       string    `json:"AccessToken"`
-	AccessTokenSecret string    `json:"AccessTokenSecret"`
-	created_at        time.Time `json:"created_at"`
-	updated_at        time.Time `json:"updated_at"`
+	Base
+	UserId             int    `json:"UserId"`
+	User               User   `gorm:"foreignKey:UserId"`
+	AccessToken        string `json:"AccessToken"`
+	AccessTokenSeacret string `json:"AccessTokenSeacret"`
 }
